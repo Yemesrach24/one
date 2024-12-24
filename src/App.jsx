fa6fde03700a7221e4e5c1980e74eadb71480bc7
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2024-12-24 18:21:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import './index.css'; 
 import React from 'react'
 
-const App = () => {
-  return (
-<div className="text-red-500">App</div> 
-  )
-}
 
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+
+  React.useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+      offset: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Category />
+      <Category2 />
+      <Services />
+      <Banner data={BannerData} />
+      <Products />
+      <Banner data={BannerData2} />
+      <Blogs />
+      <Partners />
+      <Footer />
+      <Popup orderPopup={orderPopup} handleOrderPopup={handleOrderPopup} />
+    </div>
+  );
+};
 export default App
